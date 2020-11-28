0f5af7a2b80f95029bafd74e8060007ae2b622b4
Ricardo Veloz <rick@rvlz.io>
2020-11-27 23:06:21 -0600

    remove sale/graphql

--- sales/graphql-api/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while ! nc -z sales-db 5432; do
-  sleep 0.1
-done
-
-python manage.py run -h 0.0.0.0
--- sales/graphql-api/manage.py
+++
@@ -1,9 +0,0 @@
-"""Flask CLI commands."""
-from flask.cli import FlaskGroup
-
-from app.main import create_app
-
-cli = FlaskGroup(create_app=create_app)
-
-if __name__ == "__main__":
-    cli()
